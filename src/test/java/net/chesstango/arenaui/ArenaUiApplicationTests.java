240afdb2443f7cba3a15c3f8d47ea2844ee23a00
Mauricio Coria <coriamauricio@gmail.com>
2024-12-15 21:38:45 -0300

    Initial commit

---
+++ src/test/java/net/chesstango/arenaui/ArenaUiApplicationTests.java
@@ -0,0 +1,15 @@
+package net.chesstango.arenaui;
+
+import org.junit.jupiter.api.Test;
+
+
+/**
+ * @author Mauricio Coria
+ */
+class ArenaUiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
